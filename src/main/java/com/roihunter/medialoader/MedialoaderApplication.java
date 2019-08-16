fac28b21c57851029d972d7ab6447fb615551ec4
Clayton R. Mendonça <claytonrmendonca@gmail.com>
2019-08-16 20:08:22 -0300

    Fist commit (creating a repository)

---
+++ src/main/java/com/roihunter/medialoader/MedialoaderApplication.java
@@ -0,0 +1,13 @@
+package com.roihunter.medialoader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedialoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedialoaderApplication.class, args);
+	}
+
+}
