bf1732efd4202b823470914feae74107c2c2d4ef
Clayton R. Mendonça <claytonrmendonca@gmail.com>
2019-08-17 18:46:52 -0300

    Retrieving user photos

--- src/main/java/com/roihunter/medialoader/service/UserService.java
+++ src/main/java/com/roihunter/medialoader/service/UserService.java
@@ -1,9 +1,12 @@
 package com.roihunter.medialoader.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.roihunter.medialoader.client.GraphAPI;
+import com.roihunter.medialoader.domain.Photo;
 import com.roihunter.medialoader.domain.User;
 import com.roihunter.medialoader.domain.facebook.FacebookUser;
 
@@ -13,21 +16,22 @@ public class UserService {
 	@Autowired
 	private GraphAPI graphApi;
 	
+	@Autowired
+	private PhotoService photoService;
+	
+	
 	public User create(final String token) {
-		
-		//fields=id, link, album, images, from
-		//{photo-id}/reactions?summary=total_count
-		
-		final String[] fields = new String[] {"gender", "picture", "name"};
+
+		// {photo-id}/reactions?summary=total_count
+
+		final String[] fields = new String[] { "gender", "picture", "name" };
 		final FacebookUser facebookUser = graphApi.getProfileInfo(fields, token);
-		
-		final User user = new User(
-				facebookUser.getId(), 
+		final List<Photo> userPhotos = photoService.getUserPhotos(token);
+
+		final User user = new User(facebookUser.getId(), 
 				facebookUser.getName(), 
-				facebookUser.getGender(),
-				null
-			);
-		
+				facebookUser.getGender(), 
+				userPhotos);
 
 		return user;
 	}
--- src/test/java/com/roihunter/medialoader/service/UserServiceTest.java
+++ src/test/java/com/roihunter/medialoader/service/UserServiceTest.java
@@ -36,6 +36,9 @@ public class UserServiceTest {
 	@Mock
 	private GraphAPI graphApi;
 	
+	@Mock
+	private PhotoService photoService;
+	
 	private static ObjectMapper mapper;
 	
 	@BeforeClass
@@ -61,6 +64,21 @@ public class UserServiceTest {
 		assertThat(user.getId()).isEqualTo(expectedUser.getId());
 		assertThat(user.getName()).isEqualTo(expectedUser.getName());
 		assertThat(user.getGender()).isEqualTo(expectedUser.getGender());
+	}
+	
+	@Test
+	public void create_shouldFillUserInfoWithPhotosCallingPhotoService() throws JsonParseException, JsonMappingException, IOException {
+		final String accessToken = "EAAj259ZBvk6wBABiGRWYOvAGk7gMGwZDZD";
+		final User expectedUser = mapper.readValue(Util.readJsonFile("sampleUserWithoutReactions.json"), User.class);
+		final FacebookUser facebookUser = mapper.readValue(Util.readJsonFile("sampleFacebookUserData.json"), FacebookUser.class);
+		given(graphApi.getProfileInfo(new String[] {"gender", "picture", "name"}, accessToken)).willReturn(facebookUser);
+		given(photoService.getUserPhotos(accessToken)).willReturn(expectedUser.getPhotos());
+		
+		final User user = service.create(accessToken);
+		
+		verify(photoService).getUserPhotos(accessToken);
+		
+		assertThat(user).isEqualTo(expectedUser);
 	}
 
 }
